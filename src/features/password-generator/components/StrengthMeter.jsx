f36dcef6e5a58e416d9f07174ee1e98460656c89
Julio Anccasi <julio.anccasi.dev@gmail.com>
2026-02-23 16:44:48 -0500

    Enhance strength meter component with strength labels and improved styles for better user feedback

--- src/features/password-generator/components/StrengthMeter.jsx
+++ src/features/password-generator/components/StrengthMeter.jsx
@@ -1,22 +1,32 @@
 export const StrengthMeter = ({ count }) => {
+  let strengthLabel = '';
+
+  if (count === 1 || count === 2) strengthLabel = 'Weak';
+  if (count === 3) strengthLabel = 'Medium';
+  if (count === 4) strengthLabel = 'Strong';
+
   return (
     <div className="generator__strength-container">
       <div className="generator__strength">
         <p>Strength</p>
 
-        <div className="generator__strength-bars">
-          <div
-            className={count > 0 ? 'generator__strength-bar--filled' : ''}
-          ></div>
-          <div
-            className={count > 1 ? 'generator__strength-bar--filled' : ''}
-          ></div>
-          <div
-            className={count > 2 ? 'generator__strength-bar--filled' : ''}
-          ></div>
-          <div
-            className={count > 3 ? 'generator__strength-bar--filled' : ''}
-          ></div>
+        <div className="generator__strength-indicator">
+          <span className="generator__strength-label">{strengthLabel}</span>
+
+          <div className="generator__strength-bars">
+            <div
+              className={count > 0 ? 'generator__strength-bar--filled' : ''}
+            ></div>
+            <div
+              className={count > 1 ? 'generator__strength-bar--filled' : ''}
+            ></div>
+            <div
+              className={count > 2 ? 'generator__strength-bar--filled' : ''}
+            ></div>
+            <div
+              className={count > 3 ? 'generator__strength-bar--filled' : ''}
+            ></div>
+          </div>
         </div>
       </div>
     </div>
